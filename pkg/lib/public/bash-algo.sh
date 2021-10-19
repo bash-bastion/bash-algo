bd7d43a0919eb7e143d24b5fab071cf3959637c3
Edwin Kofler <edwin@kofler.dev>
2021-10-18 20:56:17 -0700

    style: Improve code readability

--- pkg/lib/public/bash-algo.sh
+++ pkg/lib/public/bash-algo.sh
@@ -5,51 +5,40 @@ algo.base64encode() {
 	local input="$1"
 
 	local char_str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-	local has_{second,third}_byte=
-	local {first,second,third}_{byte,char}=
+	local input_byte_{one,two,three}=
+	local bits_{one,two,three,four}=
+	local output_byte_{one,two,three_four}
 	for ((i=0; i<${#input}; i=i+3)); do
-		has_second_byte=
-		has_third_byte=
+		# If there are only two bytes left, the value of third_byte and
+		# fourth_byte will both be 0
+		printf -v input_byte_one '%d' "'${input:$i:1}"
+		printf -v input_byte_two '%d' "'${input:$i+1:1}"
+		printf -v input_byte_three '%d' "'${input:$i+2:1}"
 
-		# These branches execute if the string's length isn't divisible by 3. i.e., it
-		# is divisible by 2 or only 1 (has lengths of 4, or 5). The i+1 is skipped because
-		# it will always be true due to the condition in the for loop
-		printf -v first_byte '%d' "'${input:$i:1}"
+		# Output byte one
+		bits_one=$(( (input_byte_one >> 2) & 2#00111111 ))
+		output_byte_one="${char_str:$bits_one:1}"
 
-		# TODO: can simplify if empty string results in 0
-		if ((i+2 > ${#input})); then
-			second_byte=$((2#00000000))
+		# Output byte two
+		bits_two=$(( ((input_byte_one & 2#00000011) << 4) | ((input_byte_two & 2#11110000) >> 4 & 2#00001111) ))
+		output_byte_two="${char_str:$bits_two:1}"
+
+		# Output byte three
+		if ((input_byte_two == 0)); then
+			output_byte_three='='
 		else
-			has_second_byte=yes
-			printf -v second_byte '%d' "'${input:$i+1:1}"
+			bits_three=$(( (input_byte_two & 2#00001111) << 2 | input_byte_three >> 6 & 2#00000011 ))
+			output_byte_three="${char_str:$bits_three:1}"
 		fi
 
-		if ((i+3 > ${#input})); then
-			third_byte=$((2#00000000))
+		# Output byte four
+		if ((input_byte_three == 0)); then
+			output_byte_four='='
 		else
-			has_third_byte=yes
-			printf -v third_byte '%d' "'${input:$i+2:1}"
+			bits_four=$(( input_byte_three & 2#00111111 ))
+			output_byte_four="${char_str:$bits_four:1}"
 		fi
 
-		new_first_bits=$(( (first_byte >> 2) & 2#00111111 ))
-		new_second_bits=$(( ((first_byte & 2#00000011) << 4) | ((second_byte & 2#11110000) >> 4 & 2#00001111) ))
-		new_third_bits=$(( (second_byte & 2#00001111) << 2 | third_byte >> 6 & 2#00000011 ))
-		new_fourth_bits=$(( third_byte & 2#00111111 ))
-
-		first_char="${char_str:$new_first_bits:1}"
-		second_char="${char_str:$new_second_bits:1}"
-
-		if [ "$has_second_byte" = yes ]; then
-			third_char="${char_str:$new_third_bits:1}"
-		else
-			third_char='='
-		fi
-		if [ "$has_third_byte" = yes ]; then
-			fourth_char="${char_str:$new_fourth_bits:1}"
-		else
-			fourth_char='='
-		fi
-
-		REPLY+="$first_char$second_char$third_char$fourth_char"
+		REPLY+="${output_byte_one}${output_byte_two}${output_byte_three}${output_byte_four}"
 	done
 }
